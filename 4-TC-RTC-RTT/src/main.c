bafabaf8726e75b8ad618a3c7872ba0e4915bb9f
Guilherme <guilhermer4@al.insper.edu.br>
2022-03-17 20:09:58 -0300

    Inicia Lab 4

---
+++ 4-TC-RTC-RTT/src/main.c
@@ -0,0 +1,107 @@
+#include <asf.h>
+
+#include "gfx_mono_ug_2832hsweg04.h"
+#include "gfx_mono_text.h"
+#include "sysfont.h"
+
+/************************************************************************/
+/* defines                                                              */
+/************************************************************************/
+// LED
+#define LED_PIO      PIOC
+#define LED_PIO_ID   ID_PIOC
+#define LED_IDX      8
+#define LED_IDX_MASK (1 << LED_IDX)
+
+// BUT
+#define BUT_PIO		PIOA
+#define BUT_PIO_ID	ID_PIOA  // 10
+#define BUT_PIO_IDX	11
+#define BUT_PIO_IDX_MASK (1u << BUT_PIO_IDX)
+
+// BTN1
+#define BTN1_PIO		PIOD
+#define BTN1_PIO_ID		ID_PIOD
+#define BTN1_PIO_IDX	28
+#define BTN1_PIO_IDX_MASK (1u << BTN1_PIO_IDX)
+
+// BTN2
+#define BTN2_PIO		PIOC
+#define BTN2_PIO_ID		ID_PIOC
+#define BTN2_PIO_IDX	31
+#define BTN2_PIO_IDX_MASK (1u << BTN2_PIO_IDX)
+
+// BTN3
+#define BTN3_PIO		PIOA
+#define BTN3_PIO_ID		ID_PIOA
+#define BTN3_PIO_IDX	19
+#define BTN3_PIO_IDX_MASK (1u << BTN3_PIO_IDX)
+
+// LED1
+#define LED1_PIO		PIOA
+#define LED1_PIO_ID		ID_PIOA
+#define LED1_PIO_IDX	0
+#define LED1_PIO_IDX_MASK (1u << LED1_PIO_IDX)
+
+// LED2
+#define LED2_PIO		PIOC
+#define LED2_PIO_ID		ID_PIOC
+#define LED2_PIO_IDX	30
+#define LED2_PIO_IDX_MASK (1u << LED2_PIO_IDX)
+
+// LED3
+#define LED3_PIO		PIOB
+#define LED3_PIO_ID		ID_PIOB
+#define LED3_PIO_IDX	2
+#define LED3_PIO_IDX_MASK (1u << LED3_PIO_IDX)
+
+/************************************************************************/
+/* VAR globais                                                          */
+/************************************************************************/
+
+/************************************************************************/
+/* PROTOTYPES                                                           */
+/************************************************************************/
+
+void LED_init(Pio *pio, uint32_t ul_led_id, uint32_t ul_led_mask, int initial_state);
+void TC_init(Tc * TC, int ID_TC, int TC_CHANNEL, int freq);
+void pin_toggle(Pio *pio, uint32_t mask);
+
+/************************************************************************/
+/* Handlers                                                             */
+/************************************************************************/
+
+
+
+/************************************************************************/
+/* Funcoes                                                              */
+/************************************************************************/
+void LED_init(Pio *pio, uint32_t led_id, uint32_t led_mask, int initial_state) {
+	pmc_enable_periph_clk(led_id);
+	pio_set_output(pio, led_mask, initial_state, 0,0);
+}
+
+
+/************************************************************************/
+/* Main Code	                                                        */
+/************************************************************************/
+int main (void)
+{
+	board_init();
+	sysclk_init();
+	delay_init();
+	
+	//gfx_mono_ssd1306_init();
+	//gfx_mono_draw_filled_circle(20, 16, 16, GFX_PIXEL_SET, GFX_WHOLE);
+	//gfx_mono_draw_string("mundo", 50,16, &sysfont);
+  
+	LED_init(LED1_PIO, LED1_PIO_ID, LED1_PIO_IDX_MASK, 1);
+	LED_init(LED2_PIO, LED2_PIO_ID, LED2_PIO_IDX_MASK, 1);
+	LED_init(LED3_PIO, LED3_PIO_ID, LED3_PIO_IDX_MASK, 1);
+	
+	while(1) {
+
+			
+			
+	}
+}
